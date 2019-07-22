ec94ecde992d27ed15905f4f9c2d70a8c105045d
tomBialick <tb7150@vt.edu>
2019-07-22 13:48:27 -0400

    added react.js

---
+++ web/client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App" style={{width:'100%',height:'100%'}}>
+        <header className="App-header">
+          Tom's Chat App
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- web/routes/users.js
+++ web/routes/users.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let conf_data = require('../config.json');
+
+const promise = require('bluebird');
+const initOptions = {
+    promiseLib: promise
+};
+
+const pgp = require('pg-promise')(initOptions);
+const db = pgp({
+  user: conf_data["database"]["username"],
+  host: conf_data["database"]["host"],
+  database: conf_data["database"]["db"],
+  password: conf_data["database"]["password"],
+  port: conf_data["database"]["port"],
+});
+
+/* POST a new user */
+router.post('/createUser', function(req, res, next) {
+  var user_id;
+  let username = req.body.username;
+
+  db.query('SELECT * FROM USERS WHERE USERNAME = $1', [username]).then(data => {
+    if (results[0]) {
+      res.status(400).send("Username Already Exists");
+    }
+    else {
+      db.query( 'SELECT * FROM USERS ORDER BY ID DESC').then(results => {
+        if (!results[0]) {
+          user_id = 1;
+        }
+        else {
+          user_id = results[0].id + 1;
+        }
+
+        let password = req.body.password;
+
+        db.query('INSERT INTO APPRAISAL ( ID, USERNAME, PASSWORD) VALUES ($1, $2, $3)', [user_id, username, password]).then(results => {
+          res.status(200).send("User Profile Successfully Created")
+        }).catch(error => {
+          console.log('ERROR:', error);
+          res.status(400).send("Bad Request");
+        })
+      }).catch(error => {
+        console.log('ERROR:', error);
+        res.status(500).send("Issue Connecting to Database");
+      })
+    }
+  }).catch(error => {
+    console.log('ERROR:', error);
+    res.status(500).send("Issue Connecting to Database");
+  })
+});
+
+/* POST a new authentication credential */
+router.post('/auth', function(req, res, next) {
+  res.status(200).send('respond with a resource');
+});
+
+/* GET current authentication credential */
+router.get('/auth', function(req, res, next) {
+  res.status(200).send('respond with a resource');
 });
 
 module.exports = router;
