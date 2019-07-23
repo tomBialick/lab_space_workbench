83406472d8e33e918960203703b803b374c42600
tomBialick <tb7150@vt.edu>
2019-07-23 09:03:17 -0400

    fixed more bad symbols

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 let conf_data = require('../config.json');
 
-var mongoose = require(‘mongoose’);
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://' + conf_data["mongodb"]["user"] + ':' + conf_data["mongodb"]["pwd"] + '@localhost:27017/' + conf_data["mongodb"]["db"], {useNewUrlParser: true});
-var Message = mongoose.model(‘Message’,{ name : String, message : String})
+var Message = mongoose.model('Message',{ name : String, message : String})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
