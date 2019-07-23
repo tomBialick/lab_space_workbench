e81447968699b9d8d8622cfaa5ed78b2dec36fe8
tomBialick <tb7150@vt.edu>
2019-07-23 13:21:35 -0400

    commented out db stuff for /routes/users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 let conf_data = require('../config.json');
 
-const promise = require('bluebird');
+/*const promise = require('bluebird');
 const initOptions = {
     promiseLib: promise
 };
@@ -15,9 +15,10 @@ const db = pgp({
   database: conf_data["postgresql"]["db"],
   password: conf_data["postgresql"]["password"],
   port: conf_data["postgresql"]["port"],
-});
+});*/
 
 /* POST a new user */
+/*
 router.post('/createUser', function(req, res, next) {
   var user_id;
   let username = req.body.username;
@@ -52,7 +53,7 @@ router.post('/createUser', function(req, res, next) {
     console.log('ERROR:', error);
     res.status(500).send("Issue Connecting to Database");
   })
-});
+});*/
 
 /* POST a new authentication credential */
 router.post('/auth', function(req, res, next) {
