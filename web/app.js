ce5ff0fa06a02f7502bd239e4b7170cda9ae8350
tomBialick <tb7150@vt.edu>
2019-07-23 09:21:01 -0400

    fixed path on app.js

--- web/app.js
+++ web/app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
