5aa6de04185649e01fd9530480828e787c8816c4
tomBialick <tb7150@vt.edu>
2019-07-23 09:51:35 -0400

    added preliminary chat post method

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      chat: ""
+    };
+    this.nameChange = this.nameChange.bind(this);
+    this.chatChange = this.chatChange.bind(this);
+    this.sendChat = this.sendChat.bind(this)
+  }
+
+  nameChange(event) {
+    if (event.target.value) {
+      this.setState({name: event.target.value})
+    }
+  }
+
+  chatChange(event) {
+    if (event.target.value) {
+      this.setState({chat: event.target.value})
+    }
+  }
+
+  sendChat() {
+    let hosturl = 'https://ec2-13-58-163-102.us-east-2.compute.amazonaws.com:3001/';
+    let data = {
+      "name": this.state.name,
+      "chat": this.state.chat
+    };
+    let data_json = JSON.stringify(data)
+
+    fetch( hosturl + '/chat', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: data_json
+    }).then(response => response.json()).then((responseJson) => {
+      alert(responseJson.body)
+    })
+  }
+
+
+
+  render() {
+    return (
+      <h1>Tom's Chat App</h1>;
+      <div id = "app-area" style={{width:'100%',height:'100%'}}>
+        <div id = "chat-log-area" style={{width:'100%'}}>
+        </div>
+        <div id = "message-area" style={{width:'100%'}}>
+          <form>
+            <label>Name:
+              <input type="text" onChange={this.nameChange}/>
+            </label>
+            <button onClick={this.sendChat}>Send</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- web/routes/index.js
+++ web/routes/index.js
@@ -13,4 +13,9 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/..', '/client/build', 'index.html'));
 });
 
+router.post('/chat', function(req, res, next) {
+  console.log(req.body)
+  res.status(200).json({body: {message: "chat recieved"}});
+});
+
 module.exports = router;
