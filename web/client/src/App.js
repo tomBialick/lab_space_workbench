95fc19898105cccafe8c19f89535be59eeba607c
tomBialick <tb7150@vt.edu>
2019-07-24 16:14:36 -0400

    added chat outline padding

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
         this.state.messages.map(messageData => {
           return messageData.map(dataItem => (
             <React.Fragment key={dataItem.message_id}>
-              <div style={{border: '2px solid red'}}>
+              <div style={{border: '2px solid red',padding-bottom: 10px;}}>
                 <h4>{dataItem.username}</h4>
                 <p>{dataItem.message}</p>
               </div>
