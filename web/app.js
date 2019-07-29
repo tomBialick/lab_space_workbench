fa035878edbbd78b2b904c4fe8aef8ef451dffa7
tomBialick <tb7150@vt.edu>
2019-07-29 12:09:40 -0400

    added express-fileupload

--- web/app.js
+++ web/app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,8 +11,12 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
--- web/client/src/ChatRoom.js
+++ web/client/src/ChatRoom.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './ChatRoom.css';
-import FormData from 'FormData';
 
 import socketIOClient from 'socket.io-client';
 
